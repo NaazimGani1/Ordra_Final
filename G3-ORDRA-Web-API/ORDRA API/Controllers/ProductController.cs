879554c5b878c38a0ae87dc6418362329de1effa
JGMon <jessica.gina.mon@gmail.com>
2020-09-08 23:58:58 +0200

    Product Controller

---
+++ G3-ORDRA-Web-API/ORDRA API/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Dynamic;
+using System.Data.Entity;
+using System.Web.Http.Cors;
+using ORDRA_API.Models;
+
+
+namespace ORDRA_API.Controllers
+{
+    public class ProductController : ApiController
+    {
+    }
+}
