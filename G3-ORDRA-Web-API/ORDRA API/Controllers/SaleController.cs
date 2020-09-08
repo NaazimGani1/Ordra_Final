82a24059ebfb5fa2f40e8aacf5346e3f082724fb
Ashley-Shumirayi <ashleygwa@gmail.com>
2020-09-08 14:59:28 +0200

    Subsystem 4,5,6,16 controllers

---
+++ G3-ORDRA-Web-API/ORDRA API/Controllers/SaleController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Dynamic;
+using System.Data.Entity;
+using System.Web.Http.Cors;
+using ORDRA_API.Models;
+
+namespace ORDRA_API.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+
+    [RoutePrefix("API/Sale")]
+    public class SaleController : ApiController
+    {
+        //DATABASE INITIALIZING
+        OrdraDBEntities db = new OrdraDBEntities();
+
+        //Make Sale
+        [HttpPost]
+        [Route("initiateMakeSale")]
+        public object initiateMakeSale()
+        {
+           db.Configuration.ProxyCreationEnabled = false;
+           dynamic toReturn = new ExpandoObject();
+            try
+            {
+
+            }
+            catch (Exception error)
+            {
+                toReturn = error.Message;
+            }
+
+            return toReturn;
+
+
+        }
+    //Make Sale
+        [HttpPost]
+        [Route("makeSale")]
+        public object makeSale()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            dynamic toReturn = new ExpandoObject();
+            try
+            {
+
+            }
+            catch (Exception error)
+            {
+                toReturn = error.Message;
+            }
+
+            return toReturn;
+        }
+
+
+    }
+}
