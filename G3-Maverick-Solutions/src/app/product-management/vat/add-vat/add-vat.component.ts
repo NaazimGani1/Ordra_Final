4c4e189ed204d9cf75795425bca331e874600285
JGMon <jessica.gina.mon@gmail.com>
2020-09-09 04:39:26 +0200

    Update Vat

--- G3-Maverick-Solutions/src/app/product-management/vat/add-vat/add-vat.component.ts
+++ G3-Maverick-Solutions/src/app/product-management/vat/add-vat/add-vat.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; 
+import { ProductService } from '../../product.service';
+import { Vat } from '../../vat';
 
 @Component({
   selector: 'app-add-vat',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddVatComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productService: ProductService, private router: Router) { }
+
+  vat : Vat = new Vat();
+  responseMessage: string = "Request Not Submitted";
 
   ngOnInit(): void {
+  }
+  Save(){
+    this.productService.addVat(this.vat).subscribe( (res: any)=> {
+      console.log(res);
+      if(res.Message){
+        this.responseMessage = res.Message;}
+        alert(this.responseMessage)
+        this.router.navigate(["product-management"])
+    })
+  }
+
+  Cancel(){
+    this.router.navigate(["product-management"])
   }
 
 }
--- G3-Maverick-Solutions/src/app/product-management/vat/update-vat/update-vat.component.ts
+++ G3-Maverick-Solutions/src/app/product-management/vat/update-vat/update-vat.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductService } from '../../product.service';
+import { Vat } from '../../vat';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-update-vat',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateVatComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productService: ProductService, private router: Router) { }
+  responseMessage: string = "Request Not Submitted";
 
-  ngOnInit(): void {
+  vat : Vat = new Vat();
+
+  ngOnInit() {
+    this.loadVat();
+  }
+  loadVat(){
+    this.productService.getVat().subscribe((res:any)=> {
+      console.log(res);
+      this.vat.VATPerc = res.VATPerc;
+      this.vat.VATStartDate = res.VATStartDate;
+      this.vat.VATEndDate = res.VATEndDate;
+      })
+  }
+  Save(){
+    this.productService.updateVat(this.vat).subscribe( (res:any)=> {
+      console.log(res);
+      if(res.Message){
+      this.responseMessage = res.Message;}
+      alert(this.responseMessage)
+      this.router.navigate(["product-management"])
+    })
   }
 
+  Cancel(){
+    this.router.navigate(["product-management"])
+  }
 }
